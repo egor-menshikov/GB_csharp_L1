ae413b5e7f5f69c90f8d5c9f7206f2d1b613b3ff
Egor Menshikov <r3born.fs@gmail.com>
2023-03-06 00:35:42 +0300

    added Example 005 IfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Please enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mary")
+{
+    Console.WriteLine("Wow, it's Mary herself!");
+}
+else
+{
+    Console.Write("Hello ");
+    Console.WriteLine(username);
+}
