86c27d49a21bd6242f361a320fc7796c2921e0b6
Egor Menshikov <r3born.fs@gmail.com>
2023-03-24 19:19:25 +0300

    013c complete

---
+++ Example013c_RecursionFact/Program.cs
@@ -0,0 +1,11 @@
+﻿double Fact(int num)
+{
+    if (num == 1) return 1;
+    else return num * Fact(num - 1);
+}
+
+for (int i = 1; i <= 40; i++)
+{
+    Console.Write($"{i}   ");
+    Console.WriteLine(Fact(i));
+}
