6a524493c1f780fdda59ca4f7e879494b1955690
Egor Menshikov <r3born.fs@gmail.com>
2023-03-24 19:37:47 +0300

    013d Fibonacci

---
+++ Example013d_RecFibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿int Fibo(int num)
+{
+    if (num == 1 || num == 2) return 1;
+    if (num == 0) return 0;
+    else return Fibo(num - 1) + Fibo(num - 2);
+}
+
+for (int i = 0; i <= 50; i++)
+{
+    Console.Write($"{i}   ");
+    Console.WriteLine(Fibo(i));
+}
