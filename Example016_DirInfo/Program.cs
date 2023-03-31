bddc5e0f75f1e49f8978b1ffdb75412e46f2c930
Egor Menshikov <r3born.fs@gmail.com>
2023-03-31 17:20:09 +0300

    Example016_DirInfo done

---
+++ Example016_DirInfo/Program.cs
@@ -0,0 +1,26 @@
+﻿// string path = "D:/r3/docs";
+// DirectoryInfo di = new DirectoryInfo(path);
+// Console.WriteLine(di.CreationTime);
+// FileInfo[] fi = di.GetFiles();
+
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     Console.WriteLine(fi[i].Name);
+// }
+
+
+void CatalogInfo(string path, string indent = "")
+{
+ DirectoryInfo catalogs = new DirectoryInfo(path);
+ foreach (var currentCatalog in catalogs.GetDirectories())
+ {
+ Console.WriteLine($"{indent}{currentCatalog.Name}");
+ CatalogInfo(currentCatalog.FullName, indent + " ");
+ }
+ foreach (var item in catalogs.GetFiles())
+ {
+ Console.WriteLine($"{indent}{item.Name}");
+ }
+}
+string path = "D:/r3/docs";
+CatalogInfo(path);
